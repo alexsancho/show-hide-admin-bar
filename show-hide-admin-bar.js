89b3bd8faf945ae32446762de31698064e0d5adf
Alex Sancho <alex@alexsancho.name>
2012-03-07 18:59:22 +0100

    passing jslint tests

--- show-hide-admin-bar.js
+++ show-hide-admin-bar.js
@@ -1,19 +1,24 @@
-jQuery(function() {
-	jQuery("#wpadminbar").css({'margin-top': '-25px', 'cursor': 'pointer'});
+jQuery(function () {
+	$("#wpadminbar")
+		.css({'margin-top': '-25px', 'cursor': 'pointer'});
 	var timeout = null;
 
-	jQuery("#wpadminbar").hover(function() {
+	$("#wpadminbar").hover(function () {
 		if (timeout) {
 			clearTimeout(timeout);
 			timeout = null;
 		}
 
-		jQuery(this).stop(true, true).animate({ marginTop: 0 }, 'fast');
-	}, function() {
-		var menuBar = jQuery(this);
-		timeout = setTimeout(function() {
+		$(this)
+			.stop(true, true)
+			.animate({ marginTop: 0 }, 'fast');
+	}, function () {
+		var menuBar = $(this);
+		timeout = setTimeout(function () {
 			timeout = null;
-			menuBar.stop(true, true).animate({ marginTop: -25 }, 'slow');
+			menuBar
+				.stop(true, true)
+				.animate({ marginTop: -25 }, 'slow');
 		}, 500);
 	});
 });

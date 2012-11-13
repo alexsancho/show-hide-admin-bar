d93c655399ee88a8d6abac2d626381c6342d4d5c
Alex Sancho <alex@alexsancho.name>
2012-11-14 00:44:38 +0100

    Wordpress coding standards

--- show-hide-admin-bar.php
+++ show-hide-admin-bar.php
@@ -7,16 +7,14 @@
  * Version: 1.0
  * Author URI: http://alexsancho.name/
  */
-
-function show_hide_admin_bar_init()
-{
+function show_hide_admin_bar_init() {
 	global $wp_admin_bar;
 
-	if ( ! is_admin_bar_showing() or ! is_object($wp_admin_bar))
+	if ( ! is_admin_bar_showing() or ! is_object( $wp_admin_bar ) )
 		return false;
 
-	wp_enqueue_script('show_hide_admin_bar',  plugins_url('', __FILE__) . '/show-hide-admin-bar.js', array('jquery'), '1.0', true);
+	wp_enqueue_script( 'show_hide_admin_bar',  plugins_url( '', __FILE__ ) . '/show-hide-admin-bar.js', array( 'jquery' ), '1.0', true );
 }
 
 // Initiate the plugin
-add_action('init', 'show_hide_admin_bar_init');
+add_action( 'init', 'show_hide_admin_bar_init' );
